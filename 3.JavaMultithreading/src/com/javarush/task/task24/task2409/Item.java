4ceafafbf80cd7a7878c38c9e247843e3103065a
m256ax <2991236@gmail.com>
2021-03-01 22:48:56 +0700

    01.03.2021

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.task24.task2409;
-
-public interface Item {
-    public int getId();
-    public double getPrice();
-    public String getTM();
-}
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
+++
@@ -1,6 +0,0 @@
-package com.javarush.task.task24.task2409;
-
-public interface Jeans extends Item{
-    public int getLength();
-    public int getSize();
-}
